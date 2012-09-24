b08101ddd658a43c1ec0dcc98d8553c2faece97f
djh3315 <djh3315@MARTIN.se.rit.edu>
2012-09-24 11:40:04 -0400

    Initial commit to Parser.java

---
+++ src/Parser.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author djh3315
+ *
+ */
+public class Parser {
+
+	/**
+	 * @param args
+	 */
+	Editor model = new Editor();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public String isValid(String htmlinput){
+		return htmlinput;
+	}
+	public String updateView(String htmlinput){
+		return htmlinput;
+	}
+
+}
