bcb9ed648ef4fe0ce13101de3f91ca6ce603e173
djh3315 <djh3315@DAVIES.se.rit.edu>
2012-09-24 14:48:06 -0400

    Commented out broken stuff in parser and added in main to Editor

--- src/Editor.java
+++ src/Editor.java
@@ -2,13 +2,18 @@ import java.util.ArrayList;
 public class Editor {
 	private ArrayList<Session> listOfSessions;
 	private int currentsession;
-	
+	private EditorView viewGUI;
 	public Editor() {
 		super();
+		viewGUI = new EditorView();
+		viewGUI.setVisible(true);
 		listOfSessions = new ArrayList<Session>();
 		currentsession = 0;
 	}
-
+	public static void main(String[] args){
+		Editor myEditor = new Editor();
+		System.out.println("done");
+	}
 	public void setCurrentsession(int currentsession) {
 		this.currentsession = currentsession;
 	}
